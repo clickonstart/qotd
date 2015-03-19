61e1de2dd55946e6b71770989e61c6fe173015fa
clickonstart <dev@clickonstart.com>
2015-03-19 03:12:57 -0400

    Chapter 1 Work

---
+++ grails-app/controllers/qotd/QuoteController.groovy
@@ -0,0 +1,13 @@
+package qotd
+
+class QuoteController {
+
+	static scaffold = true
+
+	def quoteService
+	
+	def random = {
+		def randomQuote = quoteService.getRandomQuote()
+		[ quote : randomQuote ]
+	}
+}
